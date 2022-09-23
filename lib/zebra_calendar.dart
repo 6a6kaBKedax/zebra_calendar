a16a3ab80b2ecb42fa5126da0fd30750e822e025
Ivan Vielkov <56674485+6a6kaBKedax@users.noreply.github.com>
2022-09-23 11:51:14 +0300

    GridView implimentation

--- lib/zebra_calendar.dart
+++ lib/zebra_calendar.dart
@@ -1,7 +1,6 @@
 library zebra_calendar;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiver/time.dart';
 
@@ -19,13 +18,13 @@ class ZebraCalendar extends StatelessWidget {
     this.textStyle,
     this.removeDayNames = false,
   }) {
-    _initCurrentMonth();
     textStyle ??= GoogleFonts.roboto(
       fontWeight: FontWeight.w400,
       fontSize: 16.0,
       color: const Color(0xFF202020),
     );
     this.initDate = initDate ?? DateTime.now();
+    _initCurrentMonth();
   }
 
   late final DateTime initDate;
@@ -39,114 +38,45 @@ class ZebraCalendar extends StatelessWidget {
   final DateTime? min;
   final DateTime? max;
 
-  final List<DateTime> _mo = [];
-  final List<DateTime> _tu = [];
-  final List<DateTime> _we = [];
-  final List<DateTime> _th = [];
-  final List<DateTime> _fr = [];
-  final List<DateTime> _sa = [];
-  final List<DateTime> _su = [];
-
-  late final List<List<DateTime>> shuffledList;
+  late final List<Widget> days;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Mo', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Tu', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('We', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Th', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Fr', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Sa', style: textStyle),
-                  ),
-          ],
-        ),
-        Column(
-          children: [
-            removeDayNames
-                ? const SizedBox()
-                : Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Text('Su', style: textStyle),
-                  ),
-          ],
+    return Column(
+      children: <Widget>[
+        GridView.count(
+          crossAxisCount: 7,
+          shrinkWrap: true,
+          childAspectRatio: 2 / 3,
+          children: days,
         ),
       ],
     );
   }
 
-  ///Columns by day of week implementation
-  /*void _initCurrentMonth() {
+  void _initCurrentMonth() {
     int numberDaysInMonth = daysInMonth(initDate.year, initDate.month);
-    List<List<DateTime>> shuffledList = [];
-    List<List<DateTime>> weekCollection = [_mo, _tu, _we, _th, _fr, _sa, _su];
-    shuffledList.addAll(weekCollection.getRange(initDate.weekday - 1, 6));
-    weekCollection.removeRange(initDate.weekday - 1, 6);
-    if (weekCollection.isNotEmpty) {
-      shuffledList.addAll(weekCollection);
+    int dayOfWeekInFirstDate = DateTime(initDate.year, initDate.month, 1).weekday;
+    List<DateTime?> shuffledList = [];
+    while (dayOfWeekInFirstDate != 0) {
+      shuffledList.add(null);
+      dayOfWeekInFirstDate--;
     }
-    int counter = 0;
     for (int i = 1; i <= numberDaysInMonth; i++) {
-      shuffledList[counter].add(DateTime(initDate.year, initDate.month, i));
-      counter++;
-      if (counter == 7) {
-        counter = 0;
-      }
+      shuffledList.add(DateTime(initDate.year, initDate.month, i));
     }
-    this.shuffledList = shuffledList;
-  }*/
+    days = shuffledList.map((e) {
+      if (e == null) {
+        return const SizedBox();
+      } else {
+        return _DayWidget(
+          available: true,
+          dayData: e,
+          onTap: onTap,
+        );
+      }
+    }).toList();
+  }
 }
 
 class CalendarController {
