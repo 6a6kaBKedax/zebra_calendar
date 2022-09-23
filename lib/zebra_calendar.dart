2f599e3ffcbc17212d7240f9212b681b6088d3c5
Ivan Vielkov <56674485+6a6kaBKedax@users.noreply.github.com>
2022-09-23 16:25:10 +0300

    add test controller functionality

--- lib/zebra_calendar.dart
+++ lib/zebra_calendar.dart
@@ -1,22 +1,23 @@
 library zebra_calendar;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiver/time.dart';
 
 /// A ZebraCalendar.
-class ZebraCalendar extends StatelessWidget {
+class ZebraCalendar extends StatelessWidget with CalendarController {
   ZebraCalendar({
     super.key,
     initDate,
     this.availableDates,
-    this.notAvailableDates,
     this.onTap,
     this.controller,
     this.min,
     this.max,
     this.textStyle,
     this.removeDayNames = false,
+    this.customBuilder,
   }) {
     textStyle ??= GoogleFonts.roboto(
       fontWeight: FontWeight.w400,
@@ -24,21 +25,25 @@ class ZebraCalendar extends StatelessWidget {
       color: const Color(0xFF202020),
     );
     this.initDate = initDate ?? DateTime.now();
-    _initCurrentMonth();
+    _currentMonthYear = initDate ?? DateTime.now();
+    _initMonth(_currentMonthYear);
+    _initController(controller);
   }
 
   late DateTime initDate;
   late TextStyle? textStyle;
   late bool removeDayNames;
 
+  final Widget Function(DateTime? date, int index)? customBuilder;
   final List<DateTime>? availableDates;
-  final List<DateTime>? notAvailableDates;
   final Function(DateTime)? onTap;
   final CalendarController? controller;
   final DateTime? min;
   final DateTime? max;
 
   late final List<Widget> days;
+
+  late DateTime _currentMonthYear;
 
   @override
   Widget build(BuildContext context) {
@@ -47,50 +52,117 @@ class ZebraCalendar extends StatelessWidget {
         GridView.count(
           crossAxisCount: 7,
           shrinkWrap: true,
-          childAspectRatio: 2 / 3,
+          childAspectRatio: 2.5 / 3,
           children: days,
         ),
       ],
     );
   }
 
-  void _initCurrentMonth() {
-    int numberDaysInMonth = daysInMonth(initDate.year, initDate.month);
-    int dayOfWeekInFirstDate = DateTime(initDate.year, initDate.month, 1).weekday;
+  void _nextMonth() {
+    if (max != null && _currentMonthYear.year == max!.year && _currentMonthYear.month == max!.month) {
+      return;
+    }
+    late DateTime nextDate;
+    if (_currentMonthYear.month == 12) {
+      nextDate = DateTime(_currentMonthYear.year + 1, 1, _currentMonthYear.day);
+      _currentMonthYear = nextDate;
+    } else {
+      nextDate = DateTime(_currentMonthYear.year, _currentMonthYear.month + 1, _currentMonthYear.day);
+      _currentMonthYear = nextDate;
+    }
+    _initMonth(_currentMonthYear);
+  }
+
+  void _previousMonth() {
+    if (min != null && _currentMonthYear.year == min!.year && _currentMonthYear.month == min!.month) {
+      return;
+    }
+    late DateTime nextDate;
+    if (_currentMonthYear.month == 1) {
+      nextDate = DateTime(_currentMonthYear.year - 1, 12, _currentMonthYear.day);
+      _currentMonthYear = nextDate;
+    } else {
+      nextDate = DateTime(_currentMonthYear.year, _currentMonthYear.month - 1, _currentMonthYear.day);
+      _currentMonthYear = nextDate;
+    }
+    _initMonth(_currentMonthYear);
+  }
+
+  void _initMonth(DateTime input) {
+    int numberDaysInMonth = daysInMonth(input.year, input.month);
+    int dayOfWeekInFirstDate = DateTime(input.year, input.month, 1).weekday;
     List<DateTime?> shuffledList = [];
-    while (dayOfWeekInFirstDate != 0) {
+    while (dayOfWeekInFirstDate != 1) {
       shuffledList.add(null);
       dayOfWeekInFirstDate--;
     }
     for (int i = 1; i <= numberDaysInMonth; i++) {
-      shuffledList.add(DateTime(initDate.year, initDate.month, i));
+      shuffledList.add(DateTime(input.year, input.month, i));
     }
-    days = shuffledList.map((e) {
-      if (e == null) {
-        return const SizedBox();
-      } else {
-        return _DayWidget(
-          available: true,
-          dayData: e,
-          onTap: onTap,
-        );
+    if (customBuilder != null) {
+      for (int i = 0; i < shuffledList.length; i++) {
+        customBuilder!(shuffledList[i], i);
       }
-    }).toList();
+    } else {
+      days = shuffledList.map((e) {
+        if (e == null) {
+          return const SizedBox();
+        } else {
+          if (availableDates != null && availableDates!.contains(e)) {
+            return _DayWidget(
+              available: true,
+              dayData: e,
+              onTap: onTap,
+            );
+          } else if (availableDates != null) {
+            return _DayWidget(
+              available: false,
+              dayData: e,
+              onTap: onTap,
+            );
+          } else {
+            return _DayWidget(
+              available: true,
+              dayData: e,
+              onTap: onTap,
+            );
+          }
+        }
+      }).toList();
+    }
+  }
+
+  void _initController(CalendarController? controller) {
+    if (controller != null) {
+      controller.nextMonth;
+    }
+  }
+
+  @override
+  void nextMonth() {
+    _nextMonth();
+  }
+
+  @override
+  void previousMonth() {
+    _previousMonth();
+  }
+
+  @override
+  DateTime currentCalendarData() {
+    return _currentMonthYear;
   }
 }
 
 class CalendarController {
-  final Function(DateTime)? currentDate;
-  final DateTime? setDate;
-  final Function()? nextMonth;
-  final Function()? nextYear;
+  void nextMonth() {}
 
-  CalendarController(
-    this.currentDate,
-    this.setDate,
-    this.nextMonth,
-    this.nextYear,
-  );
+  void previousMonth() {}
+
+  DateTime currentCalendarData() {
+    return DateTime.now();
+  }
 }
 
 class _DayWidget extends StatelessWidget {
